c3a2779d552acbf976de3011169c83f39492d90f
Tony Park <tonypark0403@gmail.com>
2020-04-16 16:19:03 -0400

    implemented updating profile image

--- src/main/java/com/tony/meeting/settings/Profile.java
+++ src/main/java/com/tony/meeting/settings/Profile.java
@@ -3,15 +3,21 @@ package com.tony.meeting.settings;
 import com.tony.meeting.domain.Account;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.BeanUtils;
 
 @Data
 @NoArgsConstructor
 public class Profile {
+    @Length(max = 35)
     private String bio;
+    @Length(max = 50)
     private String url;
+    @Length(max = 50)
     private String occupation;
+    @Length(max = 50)
     private String location;
+    private String profileImage;
     public Profile(Account account) {
         BeanUtils.copyProperties(account, this);
     }
