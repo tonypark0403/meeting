499c6c28ccd96e9d3315c2a0296a345a30efc6f7
Tony Park <tonypark0403@gmail.com>
2020-04-16 15:19:08 -0400

    added test case for profile update

---
+++ src/test/java/com/tony/meeting/WithAccountSecurityContextFactory.java
@@ -0,0 +1,35 @@
+package com.tony.meeting;
+
+import com.tony.meeting.account.AccountService;
+import com.tony.meeting.account.SignUpForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+@RequiredArgsConstructor
+public class WithAccountSecurityContextFactory implements WithSecurityContextFactory<WithAccount> {
+
+    private final AccountService accountService;
+
+    @Override
+    public SecurityContext createSecurityContext(WithAccount withAccount) {
+        String nickname = withAccount.value();
+
+        //add test user
+        SignUpForm signUpForm = new SignUpForm();
+        signUpForm.setNickname(nickname);
+        signUpForm.setEmail(nickname + "@test.com");
+        signUpForm.setPassword("test");
+        accountService.processNewAccount(signUpForm);
+
+        UserDetails principal = accountService.loadUserByUsername(nickname);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authentication);
+        return context;
+    }
+}
---
+++ src/test/java/com/tony/meeting/settings/SettingsControllerTest.java
@@ -0,0 +1,86 @@
+package com.tony.meeting.settings;
+
+import com.tony.meeting.WithAccount;
+import com.tony.meeting.account.AccountRepository;
+import com.tony.meeting.account.AccountService;
+import com.tony.meeting.account.SignUpForm;
+import com.tony.meeting.domain.Account;
+import org.aspectj.lang.annotation.After;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.TestExecutionEvent;
+import org.springframework.security.test.context.support.WithSecurityContext;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SettingsControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    AccountRepository accountRepository;
+
+    @AfterEach
+    void afterEach() {
+        accountRepository.deleteAll();
+    }
+
+    @WithAccount("test")
+    @DisplayName("Update profile form")
+    @Test
+    void updateProfileForm() throws Exception {
+        String bio = "case of updating bio";
+        mockMvc.perform(get(SettingsController.SETTINGS_PROFILE_URL))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"));
+    }
+
+    @WithAccount("test")
+    @DisplayName("Update profile - normal input")
+    @Test
+    void updateProfile() throws Exception {
+        String bio = "case of updating bio";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                .param("bio", bio)
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(SettingsController.SETTINGS_PROFILE_URL))
+                .andExpect(flash().attributeExists("message"));
+
+        Account test = accountRepository.findByNickname("test");
+        assertEquals(bio, test.getBio());
+    }
+
+    @WithAccount("test")
+    @DisplayName("Update profile - too long input")
+    @Test
+    void updateProfile_error() throws Exception {
+        String bio = "case of updating bio more than 35 ...................................................................................................";
+        mockMvc.perform(post(SettingsController.SETTINGS_PROFILE_URL)
+                .param("bio", bio)
+                .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SettingsController.SETTINGS_PROFILE_VIEW_NAME))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+                .andExpect(model().hasErrors());
+
+        Account test = accountRepository.findByNickname("test");
+        assertNull(test.getBio());
+    }
+}
